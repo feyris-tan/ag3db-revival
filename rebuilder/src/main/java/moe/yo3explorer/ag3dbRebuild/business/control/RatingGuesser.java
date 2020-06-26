bd96497c6fc20906e932d8e76dceeee175ccd274
Feyris-Tan <ft@feyris-tan.lebtimnetz.de>
2020-06-26 21:30:28 +0200

    Instead of using distribution functions, switched to a dumb brute-force approach the guess the ratings.

--- rebuilder/src/main/java/moe/yo3explorer/ag3dbRebuild/business/control/RatingGuesser.java
+++ rebuilder/src/main/java/moe/yo3explorer/ag3dbRebuild/business/control/RatingGuesser.java
@@ -4,41 +4,41 @@ import moe.yo3explorer.ag3dbRebuild.business.entity.DbCharacter;
 import moe.yo3explorer.ag3dbRebuild.business.entity.ExtractedRating;
 import org.apache.commons.math3.distribution.ExponentialDistribution;
 import org.apache.commons.math3.distribution.PoissonDistribution;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RatingGuesser
 {
-    private int slips;
-    public List<ExtractedRating> generateRatings(DbCharacter character)
-    {
-        slips = 0;
-        int maxRatings = Math.max(30,character.hits);
+    private static Logger logger;
 
+    public RatingGuesser()
+    {
+        if (logger == null)
+        {
+            logger = LogManager.getLogger(getClass());
+            logger.info("RatingGuesser has joined the action!");
+        }
+    }
+
+    public List<ExtractedRating> generateRatings(@NotNull DbCharacter character)
+    {
         double target = round(character.rating);
-        double current = Double.NaN;
+        if (Double.isNaN(target))
+        {
+            target = character.rating;
+        }
 
         if (target == 0)
             return Collections.emptyList();
 
-        LinkedList<ExtractedRating> ratings = new LinkedList<>();
-        PoissonDistribution poissonDistribution = new PoissonDistribution(target,6.0f);
-
-        do {
-            if (ratings.size() > maxRatings) {
-                ratings.clear();
-                slips++;
-            }
-            int sample = poissonDistribution.sample();
-            int tba = sample;
-            tba = clamp(tba,1,5);
-            ratings.add(ExtractedRating.createGuess(character,tba));
-            current = ratings.stream().mapToDouble(x -> (double)x.ratingValue).average().getAsDouble();
-            current = round(current);
-        } while (target != current);
-        return ratings;
+        int[] ratings = search(character.rating);
+        List<ExtractedRating> collect = Arrays.stream(ratings).mapToObj(x -> ExtractedRating.createGuess(character, x)).collect(Collectors.toList());
+        return collect;
     }
 
     private static double round(double value)
@@ -46,17 +46,65 @@ public class RatingGuesser
         return (double)Math.round(value * 100000d) / 100000d;
     }
 
-    private static int clamp(int actual, int lower, int upper)
+    private int[] searchState;
+    private HashMap<Double,int[]> searchCache;
+
+    private int[] search(double target)
     {
-        if (lower > actual)
-            return lower;
-        if (upper < actual)
-            return upper;
-        return actual;
+        if (searchCache == null)
+            searchCache = new HashMap<>();
+        if (searchCache.containsKey(target))
+            return searchCache.get(target);
+
+        double searchStateAvg = Double.NaN;
+        logger.info(String.format("Searching for %f",target));
+        while (searchStateAvg != target)
+        {
+            incrementSearchState(0);
+            searchStateAvg = fastAverage(searchState);
+            searchStateAvg = round(searchStateAvg);
+            if (!searchCache.containsKey(searchStateAvg)) {
+                int[] newChain = Arrays.copyOf(searchState, searchState.length);
+                logger.info(String.format("Found chain for %.5f: (%s)",searchStateAvg,String.join("->", Arrays.stream(newChain).mapToObj(x -> Integer.toString(x)).collect(Collectors.toList()))));
+                searchCache.put(searchStateAvg, newChain);
+            }
+        }
+        return searchState;
     }
 
-    public int getSlips() {
-        return slips;
+    @Contract(pure = true)
+    private double fastAverage(int @NotNull [] values)
+    {
+        double d = 0;
+        for (int i = 0; i < values.length; i++)
+        {
+            d += values[i];
+        }
+        d /= values.length;
+        return d;
     }
 
+    private void incrementSearchState(int offset)
+    {
+        if (searchState == null)
+        {
+            searchState = new int[]{1};
+            return;
+        }
+        if (searchState.length < offset + 1)
+        {
+            int[] increasedRoom = Arrays.copyOf(searchState,offset + 1);
+            increasedRoom[offset] = 1;
+            searchState = increasedRoom;
+            logger.info(String.format("Increased search space to %d!",offset));
+            return;
+        }
+
+        searchState[offset]++;
+        if (searchState[offset] == 6)
+        {
+            searchState[offset] = 1;
+            incrementSearchState(offset + 1);
+        }
+    }
 }
--- rebuilder/src/test/java/moe/yo3explorer/ag3dbRebuild/business/control/RatingGuesserTests.java
+++ rebuilder/src/test/java/moe/yo3explorer/ag3dbRebuild/business/control/RatingGuesserTests.java
@@ -3,6 +3,7 @@ package moe.yo3explorer.ag3dbRebuild.business.control;
 import moe.yo3explorer.ag3dbRebuild.business.boundary.Ag3DbOrm;
 import moe.yo3explorer.ag3dbRebuild.business.entity.DbCharacter;
 import moe.yo3explorer.ag3dbRebuild.business.entity.ExtractedRating;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -12,16 +13,35 @@ import java.util.List;
 public class RatingGuesserTests {
     @Test
     public void testRatingGenerator() throws IOException, SQLException {
-        Ag3DbOrm ag3DbOrm = new Ag3DbOrm();
-        DbCharacter firstRowCharacter = ag3DbOrm.findFirstRowCharacter();
         RatingGuesser ratingGuesser = new RatingGuesser();
-        List<ExtractedRating> extractedRatings = ratingGuesser.generateRatings(firstRowCharacter);
-        for (ExtractedRating extractedRating : extractedRatings) {
-            System.out.print(extractedRating.ratingValue);
-            System.out.print(',');
-        }
-        System.out.println();
-        System.out.printf("%d slips\n",ratingGuesser.getSlips());
+        ratingGuesser.generateRatings(buildDummyCharacter(4.57576));
+        ratingGuesser.generateRatings(buildDummyCharacter(4.3));
+        ratingGuesser.generateRatings(buildDummyCharacter(4.29412));
+        ratingGuesser.generateRatings(buildDummyCharacter(4.1875));
+        ratingGuesser.generateRatings(buildDummyCharacter(4.16667));
+        ratingGuesser.generateRatings(buildDummyCharacter(4.14286));
+        ratingGuesser.generateRatings(buildDummyCharacter(4));
+        ratingGuesser.generateRatings(buildDummyCharacter(3.88889));
+        ratingGuesser.generateRatings(buildDummyCharacter(3.85714));
+        ratingGuesser.generateRatings(buildDummyCharacter(3.83333));
+        ratingGuesser.generateRatings(buildDummyCharacter(3.82353));
+        ratingGuesser.generateRatings(buildDummyCharacter(3.8));
+        ratingGuesser.generateRatings(buildDummyCharacter(3.75));
+        ratingGuesser.generateRatings(buildDummyCharacter(2.875));
+        ratingGuesser.generateRatings(buildDummyCharacter(2.33333));
+        ratingGuesser.generateRatings(buildDummyCharacter(2.25));
+        ratingGuesser.generateRatings(buildDummyCharacter(2.2));
+        ratingGuesser.generateRatings(buildDummyCharacter(2.11111));
+        ratingGuesser.generateRatings(buildDummyCharacter(1.8));
+        ratingGuesser.generateRatings(buildDummyCharacter(1.75));
+        ratingGuesser.generateRatings(buildDummyCharacter(1.6));
+        ratingGuesser.generateRatings(buildDummyCharacter(1));
+    }
 
+    private @NotNull DbCharacter buildDummyCharacter(double rating)
+    {
+        DbCharacter dbCharacter = new DbCharacter();
+        dbCharacter.rating = rating;
+        return dbCharacter;
     }
 }
